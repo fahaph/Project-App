3c2a2cdb10d7b793f2fe5a9329f75421d629456e
fahaph <F_Phonwen@hotmail.com>
2024-09-24 10:57:49 +0700

    #

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,26 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screen/latestRate.dart';
-import 'package:flutter_application_1/screen/convert.dart';
+import 'package:flutter_application_1/screen/form_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/provider/transaction_provider.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MyApp());
+}
 
-    debugShowCheckedModeBanner: false,
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-    home: Scaffold(
-      appBar: AppBar(
-        
-        title: Center(child: Text('Exchange Currency', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),),
-        backgroundColor:Color.fromARGB(255, 51, 184, 255 ),
-
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return TransactionProvider();
+        }),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
+        home: const MyHomePage(title: 'แอพบัญชี'),
       ),
-      body: const LatestRate(),
-    ),
-  ));
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return FormScreen();
+                }));
+              },
+            ),
+          ],
+        ),
+        body: Consumer(
+          builder: (context, TransactionProvider provider, Widget? child) {
+            return ListView.builder(
+              itemCount: provider.transactions.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                  child: ListTile(
+                    title: Text(provider.transactions[index].title),
+                    subtitle: Text(provider.transactions[index].date.toString()),
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: FittedBox(
+                        child: Text('${provider.transactions[index].amount}'),
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {},
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
 }
